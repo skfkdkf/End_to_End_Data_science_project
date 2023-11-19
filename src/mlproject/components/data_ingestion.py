6f7994b767775514a06560c12625ddb7ebfcf49a
Shubham6364 <projectfull3114@gmail.com>
2023-11-19 14:09:54 +0530

    EDA

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -6,7 +6,6 @@ import pandas as pd
 from src.mlproject.utils import read_sql_data
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-import csv
 
 
 @dataclass
