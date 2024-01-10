1a600fa1b60c4cf848bc6d823dd81a87a33a6cc9
Shubham6364 <projectfull3114@gmail.com>
2024-01-10 21:59:39 +0530

    preduction and deployment in Flask is incompleted

---
+++ src/mlproject/pipelines/prediction_pipeline.py
@@ -0,0 +1,35 @@
+import sys
+import pandas as pd
+from src.mlproject.exception import CustomException
+from src.mlproject.utils import load_object
+
+
+class PredictPipline:
+    def __init__(self):
+        pass
+    
+
+class CustomData:
+    def __init__(self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int):
+        
+        self.gender = gender
+
+        self.race_ethnicity = race_ethnicity
+
+        self.parental_level_of_education = parental_level_of_education
+
+        self.lunch = lunch
+
+        self.test_preparation_course = test_preparation_course
+
+        self.reading_score = reading_score
+
+        self.writing_score = writing_score
+    
